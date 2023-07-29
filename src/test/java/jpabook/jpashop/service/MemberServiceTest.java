2d6e9e18177a78ee1629fd9d0b9bd39d4c1342bc
donghyuun <bomul0524@gmail.com>
2023-07-30 03:48:39 +0900

    회원 기능 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest //스프링과 같이 테스트하기 위해
+@Transactional //rollback 시키기 위해
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Test
+    public void 회원가입() throws Exception {
+        //given 이런게 주어졌을때
+        Member member = new Member();
+        member.setName("kim");
+
+        //when 이렇게 하면
+        Long savedId = memberService.join(member);
+
+        //then 이렇게 되야 한다
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    public void 중복_회원_예약() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+    }
+}
