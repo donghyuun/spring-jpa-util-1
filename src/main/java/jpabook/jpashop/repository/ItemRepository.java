34f37e797265ef241e4ca38e39890fb8091ff800
donghyuun <bomul0524@gmail.com>
2023-08-03 22:49:30 +0900

    상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if(item.getId() == null) {//저장되기 전까지 id값이 없으므로(완전히 새로 생성된 객체이므로) => 신규로 등록
+             em.persist(item);
+        }else{//기존에 db에 등록되어 있음 => 강제로 업데이트
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
