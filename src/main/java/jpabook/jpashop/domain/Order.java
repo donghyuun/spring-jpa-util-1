b979ed1067c42da7fa8cde37f4802da809185804
donghyuun <bomul0524@gmail.com>
2023-08-03 23:46:01 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.aspectj.weaver.ast.Or;
 
 import java.time.LocalDateTime;
@@ -11,6 +13,7 @@ import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Table(name = "orders") //이름 없으면 테이블명이 Order가 됨
+@Getter @Setter
 public class Order {
 
     @Id @GeneratedValue // db에 자동 생성(개발자가 안 지정해줘도 됌)
@@ -48,5 +51,42 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    //주문 취소
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);// 취소 상태로 바꿔줌
+        for (OrderItem orderItem : this.orderItems) {// 루프를 돌면서 재고를 원상복구
+            orderItem.cancel();
+        }
+    }
+
+
+    //==조회 로직==//
+    //전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -21,9 +21,31 @@ public class OrderItem {
     private Item item;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "order_id")//Order 엔티티의 Column 명이 "order_id"(왜래 키) 인 것과 매핑됨, 주인O
+    @JoinColumn(name = "order_id")// Order 엔티티의 Column 명이 "order_id"(왜래 키) 인 것과 매핑됨, 주인O
     private Order order;
 
-    private int orderPrice; //주문 당시의 가격
+    private int orderPrice; // 주문 당시의 가격
     private int count; // 주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);//재고를 감소시킨다
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {// item에서의 재고수량을 원상복구 해준다
+        this.getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 }
